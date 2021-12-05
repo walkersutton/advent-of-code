b14840310f39c15abcc99d83020df836ff016847
walkersutton <walker.c.sutton@gmail.com>
2021-12-05 00:49:24 -0500

    day 5

---
+++ 2021/5/main.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append("..")
+
+from parse import input_as_string, input_as_lines, input_as_ints
+
+lines = input_as_lines('data.txt')
+
+max_x, max_y = 0, 0
+
+segments = []
+for line in lines:
+	segment = []
+	line = line.split(' -> ')
+	start = line[0].split(',')
+	start = [int(start[0]), int(start[1])]
+	end = line[1].split(',')
+	end = [int(end[0]), int(end[1])]
+	segments.append([start, end])
+	max_x = max(max_x, start[0], end[0])
+	max_y = max(max_y, start[1], end[1])
+
+board = []
+for ii in range(max_y + 1):
+	row = []
+	for jj in range(max_x + 1):
+		row.append(0)
+	board.append(row)	
+
+for s in segments:
+	start = s[0]	
+	end = s[1]
+	# vertical
+	if start[0] == end[0]:
+		for ii in range(abs(start[1] - end[1]) + 1):
+			board[min(start[1], end[1]) + ii][start[0]] += 1
+	# horizontal
+	elif start[1] == end[1]:
+		for ii in range(abs(start[0] - end[0]) + 1):
+			board[start[1]][min(start[0], end[0]) + ii] += 1
+	# diagnoal
+	else: 
+		pass
+
+count = 0
+for row in board:
+	for ele in row:
+		if ele >= 2:
+			count += 1
+print(count)
---
+++ 2021/5/readme.md
@@ -0,0 +1,11 @@
+# AOC 2021 - Day 5
+
+[instructions](https://adventofcode.com/2021/day/5)
+
+### Part 1
+
+> Count the number of points where at least 2 lines overlap. (only consider horizontal and vertical lines)
+
+### Part 2
+
+> Count the number of points where at least 2 lines overlap. (now also consider diagonal lines)
