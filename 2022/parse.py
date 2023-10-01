156289e21f02623fdb08dc2e3e0b69a2bbf2554c
walkersutton <walker.c.sutton@gmail.com>
2023-09-30 19:06:18 -0700

    add to helpers

--- 2022/parse.py
+++ 2022/parse.py
@@ -19,3 +19,23 @@ def input_as_ints(filename:str) -> List[int]:
     lines = input_as_lines(filename)
     line_as_int = lambda l: int(l.rstrip('\n'))
     return list(map(line_as_int, lines))
+
+def partition(arr, l, h, cmp_fn):
+    piv = arr[h]
+    ii = l - 1
+
+    for jj in range(l, h):
+        if cmp_fn(arr[jj], piv):
+            ii += 1
+            (arr[ii], arr[jj]) = (arr[jj], arr[ii])
+    (arr[ii + 1], arr[h]) = (arr[h], arr[ii + 1])
+
+    return ii + 1
+
+def quicksort(arr, cmp_fn, l=0, h=None):
+    if h is None:
+        h = len(arr) - 1
+    if l < h:
+        piv = partition(arr, l, h, cmp_fn)
+        quicksort(arr, cmp_fn, l, piv - 1)
+        quicksort(arr, cmp_fn, piv + 1, h)
