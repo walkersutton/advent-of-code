9d1387da07018e18bd673e4b83213364847112c3
Walker Sutton <walker.c.sutton@gmail.com>
2025-12-06 20:34:21 -0800

    update template

--- 2025/template/main.py
+++ 2025/template/main.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append(os.path.pardir)
 from parse import input_as_string, input_as_lines, input_as_ints
 
-# string = input_as_string('data.txt')
-# lines = input_as_lines('data.txt')
-# ints = input_as_ints('data.txt')
+file_name = 'data.txt'
+# string = input_as_string(file_name)
+# lines = input_as_lines(file_name)
+# ints = input_as_ints(file_name)
